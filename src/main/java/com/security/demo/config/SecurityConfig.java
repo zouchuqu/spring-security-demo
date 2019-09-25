294ac6fa8273d5d4bccd6fca4d477e79adcd71f8
weikechang@hingin.com <hingin201818>
2019-09-25 16:48:18 +0800

    SpringBoot 集成 SpringSecurity 详解（三）-- 基于内存实现多用户认证信息

---
+++ src/main/java/com/security/demo/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @auther kklu
+ * @date 2019/9/24 17:40
+ * @describe
+ */
+@Configuration
+@EnableWebSecurity//开启Spring Security的功能
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //SpringSecurity 提供的一种编码器，我们也可以自己实现PasswordEncoder
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //通过内存创建用户名和密码
+        auth.inMemoryAuthentication().withUser("user").password("123456").roles();
+        auth.inMemoryAuthentication().withUser("admin").password("123456").roles();
+    }
+
+}
