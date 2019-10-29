4cd05818c3b68c82ae9bc483fde91d277aa44208
weikechang@hingin.com <hingin201818>
2019-10-29 18:17:09 +0800

    SpringBoot 集成 SpringSecurity 详解（四）-- 基于MySQL数据库实现多用户认证信息

--- src/main/java/com/security/demo/config/SecurityConfig.java
+++ src/main/java/com/security/demo/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,7 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity//开启Spring Security的功能
-@EnableGlobalMethodSecurity(prePostEnabled=true)//开启方法安全级别的控制
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
     @Bean
